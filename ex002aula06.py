8fffb2328dd4c4f4ef555b7627147305b4ff2273
mariana regia <guirado.mariana03@gmail.com>
2022-05-02 23:50:16 -0300

    up

---
+++ ex002aula06.py
@@ -0,0 +1,27 @@
+# n = bool(input('Digite um valor'))
+# print(n)
+
+# a = input('digite algo: ')
+# print(a.isnumeric())
+
+
+# meu primeiro identificador de palavras
+# a = input('digite algo: ')
+#
+# if a.isalpha() == True:
+#     print('certo, é uma palavra')
+# else:
+#     print('infelizmente nao eh palavra')
+
+# a = input('digite algo: ')
+# print(a.isalnum())
+
+# está em letra maiúsculo?
+# a = input('digite algo: ')
+# print(a.isupper())
+
+# DESAFIO 1 AULA 06
+a
+
+
+# DESAFIO 2 AULA 06
